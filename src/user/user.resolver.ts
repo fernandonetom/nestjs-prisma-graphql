52f669b6c16a401acfb605465947b15ceae7e43d
Fernando <fernando39357@gmail.com>
2022-01-07 11:38:35 -0300

    feat: update user

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,7 @@
 import { NotFoundException } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateUserInput } from './dto/create-user.dto';
+import { UpdateUserInput } from './dto/update-user.dto';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
@@ -27,5 +28,19 @@ export class UserResolver {
   @Mutation(() => User)
   createUser(@Args('data') data: CreateUserInput): Promise<User> {
     return this.userService.createUser(data);
+  }
+
+  @Mutation(() => User)
+  async updateUser(
+    @Args('id') id: string,
+    @Args('data') data: UpdateUserInput,
+  ): Promise<User> {
+    const user = await this.userService.findUserById(id);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return this.userService.updateUser(id, data);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,21 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/services/prisma.service';
 import { CreateUserInput } from './dto/create-user.dto';
+import { UpdateUserInput } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async findAll(): Promise<User[]> {
+  findAll(): Promise<User[]> {
     return this.prisma.user.findMany();
   }
 
-  async findUserById(id: string): Promise<User> {
+  findUserById(id: string): Promise<User> {
     return this.prisma.user.findUnique({ where: { id: Number(id) } });
   }
 
-  async createUser(data: CreateUserInput): Promise<User> {
+  createUser(data: CreateUserInput): Promise<User> {
     return this.prisma.user.create({
+      data,
+    });
+  }
+
+  updateUser(id: string, data: UpdateUserInput): Promise<User> {
+    return this.prisma.user.update({
+      where: { id: Number(id) },
       data,
     });
   }
